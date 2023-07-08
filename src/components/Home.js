b08f6285e4f028ec339a61dc85f22ee139223dd1
DomenicoBarb <domenicobarberi585@gmail.com>
2023-07-08 04:51:12 -0400

    changed bg colour and intro screen text

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const { Title, Text } = Typography;
 const Home = () => {
     return (
         <div className='Home'>
-            <Title className='MainMenuAnimation' style={{ margin: 0, color: 'rgb(255, 255, 255)' }}>Welcome to the Typing Test App!</Title>
+            <Title className='MainMenuAnimation' style={{ margin: 0, color: 'rgb(255, 255, 255)' }}>Welcome to Nico's Typing Test React App!</Title>
             <Text className='menu-intro1'>A simple and fun way to test your typing skills,</Text>
             <Text className='menu-intro2'>with a word tracking highlighter to show progress!</Text>
             <Link to="/typing-test">
